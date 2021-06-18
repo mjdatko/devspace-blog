4f83be39cff93ec75b4da0e50bc1818164c87a34
mjdatko <mdatko@sbcglobal.net>
2021-06-18 16:35:49 -0700

    second commit

---
+++ components/Layout.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Header from './Header'
+import Search from './Search'
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name='keywords' content={keywords} />
+        <meta name='description' content={description} />
+
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <Header />
+      <Search />
+
+      <main className='container mx-auto my-7'>{children}</main>
+    </div>
+  )
+}
+
+Layout.defaultProps = {
+  title: 'Welcome to DevSpace',
+  keywords: 'development, coding, programming',
+  description: 'the best info and news in development',
+}
